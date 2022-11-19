e4c7e61b4a83f1159e2eba64eb6a1f6b15a74825
ArinaD <ademkhina@list.ru>
2022-11-20 00:39:09 +0700

    what's qt?

---
+++ front/main_ui.py
@@ -0,0 +1,38 @@
+from sys import argv, exit
+
+from PyQt5 import QtWidgets
+
+from back.composition import Composition
+from back.playlist import Playlist, PlaylistItem
+from front.designer_maked.des_ui import Ui_MainWindow
+
+from PyQt5.QtWidgets import QMainWindow, QApplication, QGroupBox
+
+
+class MainUI(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+
+class TrackGroupBox(QGroupBox):
+    def __init__(self, composition: Composition):
+        super().__init__()
+
+
+class CurPlaylistGroupbox(QGroupBox):
+    def __init__(self, playlist: Playlist):
+        super().__init__()
+        self.curPlaylistScrollAreaWidget = QtWidgets.QWidget()
+        self.playlist = playlist
+
+        for tracks in playlist:
+            track = TrackGroupBox(tracks.data)
+
+
+if __name__ == '__main__':
+    app = QApplication(argv)
+    ex = MainUI()
+    ex.show()
+    app.exec_()
+    exit()
