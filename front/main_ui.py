879658f379f1a16fc9a4c3f8760d3de8d28709ac
ArinaD <ademkhina@list.ru>
2022-11-26 23:01:56 +0700

    swap done

--- front/main_ui.py
+++ front/main_ui.py
@@ -12,7 +12,7 @@ from front.designer_maked.des_ui import Ui_MainWindow
 from PyQt5.QtWidgets import (QScrollArea, QApplication, QWidget,
                              QMainWindow, QHBoxLayout, QVBoxLayout,
                              QLabel, QSpacerItem, QSizePolicy,
-                             QPushButton, QTabWidget, QSlider,
+                             QPushButton, QSlider,
                              QGroupBox, QDialog, QCheckBox, QLineEdit)
 
 
@@ -41,7 +41,7 @@ class MainUI(QMainWindow, Ui_MainWindow):
         self.playLineGroupBox.hide()
 
     def update_handler(self):
-        print(f"{self.sender().name} обновился")
+        print(f"\033[0;32m[ MAIN]\033[0;0m {self.sender().name} обновился")
 
 
 class AllPlaylistsGroupbox(QGroupBox):
@@ -82,12 +82,13 @@ class PlaylistGroupbox(QGroupBox):
         self.layout.addItem(QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum))
 
     def mousePressEvent(self, event) -> None:
-        print(f"{self.name} хочет стать настоящим")
+        print(f"\033[0;34m[PLLST]\033[0;0m {self.name} хочет стать текущим")
         self.want_to_be_cur.emit()
 
 
 class TrackGroupbox(QGroupBox):
     want_play = pyqtSignal()
+    swap_dir = pyqtSignal(str)
 
     def __init__(self, composition: Composition, parent=None):
         super().__init__(parent)
@@ -97,6 +98,8 @@ class TrackGroupbox(QGroupBox):
         self.down = QPushButton("↓")
         self.up.setMaximumSize(QSize(15, 30))
         self.down.setMaximumSize(QSize(15, 30))
+        self.up.clicked.connect(self.swap_direction)
+        self.down.clicked.connect(self.swap_direction)
 
         self.layout = QHBoxLayout(self)
         self.pic = QLabel()
@@ -116,12 +119,26 @@ class TrackGroupbox(QGroupBox):
         self.layout.addWidget(self.meta_dur)
 
     def mousePressEvent(self, event) -> None:
-        print(f"{self.name} хочет играть")
+        print(f"\033[0;35m[TRACK]\033[0;0m {self.name} хочет играть")
         self.want_play.emit()
+
+    def swap_direction(self):
+        direction = self.sender().text()
+        print(f"\033[0;35m[TRACK]\033[0;0m {self.name} хочет меняться {direction}")
+        self.swap_dir.emit(direction)
+
+    # def swap_up(self):
+    #     print(f"\033[0;35m[TRACK]\033[0;0m {self.name} хочет меняться вверх")
+    #     self.swap_tracks.emit("up")
+
+    # def swap_down(self):
+    #     print(f"\033[0;35m[TRACK]\033[0;0m {self.name} хочет меняться вниз")
+    #     self.swap_tracks.emit("down")
 
 
 class CurPlaylistGroupbox(QGroupBox):
     updated = pyqtSignal()
+    list_updated = pyqtSignal(int)
 
     def __init__(self, playlist: Playlist):
         super().__init__()
@@ -139,6 +156,7 @@ class CurPlaylistGroupbox(QGroupBox):
         for song in playlist:
             new_track = TrackGroupbox(song.data, self)
             new_track.want_play.connect(self.play_slot)
+            new_track.swap_dir.connect(self.swap_tracks)
             self.track_boxes.append(new_track)
             self.scrollAreaWidgetLayout.addWidget(new_track)
 
@@ -164,9 +182,49 @@ class CurPlaylistGroupbox(QGroupBox):
         return self.playlist.name
 
     def play_slot(self):
-        print(f"Плейлист {self.playlist.name} понял что {self.sender().name} хочет играть")
+        print(f"\033[0;36m[CURPL]\033[0;0m Плейлист {self.playlist.name} понял что {self.sender().name} хочет играть")
         # self.playlist.current_track = self.sender().composition
         self.updated.emit()
+
+    def swap_tracks(self, direction):
+        print(f"\033[0;36m[CURPL]\033[0;0m Плейлист {self.playlist.name} понял что {self.sender().name} "
+              f"хочет поменяться местами {direction}")
+        track = self.sender().composition
+        self.playlist.swap(track, direction)
+        self.upd()
+
+    def upd(self, index=-1):
+        print(f"\033[0;36m[CURPL]\033[0;0m Плейлист {self.playlist.name} обновился")
+        is_filled = False
+        if self.track_boxes:
+            self.track_boxes.clear()
+            is_filled = True
+        count = self.scrollAreaWidgetLayout.count()
+        for i in range(0, count):
+            item = self.scrollAreaWidgetLayout.itemAt(i)
+            print(item.widget())
+            if i == count-1:
+                self.scrollAreaWidgetLayout.removeItem(item)
+            else:
+                item.widget().deleteLater()
+        for song in self.playlist:
+            track = TrackGroupbox(song.data)
+            track.swap_dir.connect(self.swap_tracks)
+            track.clicked.connect(self.play_slot)
+            self.scrollAreaWidgetLayout.addWidget(track)
+            self.track_boxes.append(track)
+
+        spacerItem = QtWidgets.QSpacerItem(0, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.scrollAreaWidgetLayout.addItem(spacerItem)
+
+        if not is_filled:
+            self.cur_track = self.track_boxes[0]
+
+        self.update_meta()
+        self.list_updated.emit(index)
+
+    def update_meta(self):
+        self.playlist_meta.setText(str(self.playlist))
 
 
 class PlayLineGroupBox(QGroupBox):
